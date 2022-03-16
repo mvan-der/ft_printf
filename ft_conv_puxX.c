091d788e7d915c74e639cdf4df20b6615cc86b83
Maarten Van der vlugt <mvan-der@f1r2s7.codam.nl>
2022-03-16 13:37:38 +0100

    Bit of file cleanup

--- ft_conv_puxX.c
+++ ft_conv_puxX.c
@@ -1,16 +1,37 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        ::::::::            */
-/*   ft_conv_uxX.c                                      :+:    :+:            */
+/*   ft_conv_puxX.c                                     :+:    :+:            */
 /*                                                     +:+                    */
 /*   By: mvan-der <mvan-der@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/12/13 17:36:57 by mvan-der      #+#    #+#                 */
-/*   Updated: 2021/12/22 14:00:43 by mvan-der      ########   odam.nl         */
+/*   Updated: 2022/03/16 13:34:57 by mvan-der      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
+
+void	ft_conv_p(va_list arguments)
+{
+	void				*p;
+	unsigned long int	i;
+	char				*str;
+	char				*res;
+
+	p = va_arg(arguments, void *);
+	if (p == NULL)
+	{
+		ft_putstr("0x0");
+		return ;
+	}
+	i = (unsigned long int) p;
+	str = ft_utoa_printf(i, 16, 'x');
+	res = ft_strjoin("0x", str);
+	ft_putstr(res);
+	free(str);
+	free(res);
+}
 
 void	ft_conv_u(va_list arguments)
 {
